f2797722f1903014e4a97b1263470ae903de1788
coazz <aco94@hotmail.com>
2023-02-07 21:56:44 +0100

    upload

--- assigments.js
+++ assigments.js
@@ -4,27 +4,39 @@ function fibo(num){
     return (num <= 0) ? "Please use a positive integer"
         : (num === 0) ? [0]
         : (num === 1) ? [0,1]
+        // prevArray + prevArray.at(-1) + prevArray.at(-2)
         : fibo(num - 1).concat(fibo(num - 1).at(-1) + fibo(num - 1).at(-2))
 
 }
 
 // Assignment 2
-function mergeSort(array){
-    if (array.length <= 1) return array;
-
-    let right = array.slice(0, (array.length / 2));
-    let left = array.slice((array.length / 2));
-
-    return merge(mergeSort(right), mergeSort(left))
-}
-
-
 function merge(left, right) {
     let sum = [];
 
-    while(left.length > 0 && right.length > 0){
+    // so lange keines der array empty ist vergleiche die Zahlen
+    while(left.length > 0 && right.length > 0) {
+        // die kleinse Zahl ergibt sich aus dem vergleich Links > Recht, wenn true dann geht Rechts in die Summe, andersrum geht Links in die Summe
         let smallest = left[0] > right[0] ? right.shift() : left.shift();
-        sum.push(smallest);
+        sum.push(smallest)
     }
-    return sum.concat(right, left)
+
+    // nach dem Vergleich wird es immer ein Array mit einer Nummber oder mehreren geben, wir wollen aber auch diese in der Summe haben
+    const remainingNumbers = left[0] ? left : right;
+
+    return sum.concat(remainingNumbers)
+}
+
+
+const mergeSort = (array) => {
+
+    // wenn array nur eine Zahl soll man dieses Array zurück geben
+    if (array.length < 2) return array;
+
+    // Halbiere Array in 2 Helften
+    const half = Math.floor(array.length / 2);
+    const left = array.slice(0, half);
+    const right = array.slice(half, array.length);
+
+    // merge diese 2 Helften
+    return merge(mergeSort(left), mergeSort(right))
 }
--- binarytree.js
+++ binarytree.js
@@ -1,124 +1,3 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
-  }
-}
-
-class Tree {
-  constructor(array) {
-    this.array = mergeSort(removeDuplicates(array));
-    this.root = this.buildTree(this.array);
-  }
-
-  buildTree(array) {
-    if (array.length < 1) return null;
-
-    const middlePoint = Math.floor((array.length - 1) / 2);
-    const left = array.slice(0, middlePoint);
-    const right = array.slice(middlePoint + 1);
-    const node = new Node(array[middlePoint]);
-
-    node.right = this.buildTree(right);
-    node.left = this.buildTree(left);
-
-    return node;
-  }
-
-  insert(value) {
-    // Check if the value is 0 or undefined and return errro
-    if (value === 0 || value === undefined)
-      return console.error("0, minus integers and undefined arent allowed");
-
-    let temp = this.root;
-    let previous = null;
-
-    //
-    while (temp !== null) {
-      if (temp.data === value)
-        return console.error("Value exists already in the Tree");
-      previous = temp;
-      temp = temp.data > value ? temp.left : temp.right;
-    }
-
-    if (previous.data > value) {
-      previous.left = new Node(value);
-    } else {
-      previous.right = new Node(value);
-    }
-  }
-
-  delete(value) {
-    // Check if the value is 0 or undefined and return errro
-    if (value === 0 || value === undefined)
-      return console.error("0, minus integers and undefined arent allowed");
-
-    let temp = this.root;
-    let previous = null;
-
-    while (temp !== null) {
-      if (temp.data === value) break;
-      previous = temp;
-      temp = temp.data > value ? temp.left : temp.right;
-    }
-
-    if (previous.data > value) {
-      previous.left = null;
-    } else {
-      previous.right = null;
-    }
-  }
-
-  find(value) {
-    // Check if the value is 0 or undefined and return errro
-    if (value === 0 || value === undefined)
-      return console.error("0, minus integers and undefined arent allowed");
-
-    let temp = this.root;
-    let previous = null;
-
-    while (temp !== null) {
-      if (temp.data === value) break;
-      previous = temp;
-      temp = temp.data > value ? temp.left : temp.right;
-    }
-
-    return temp;
-  }
-
-  leveOrder(root){
-    // add root.data to array, go left and right, save left than right, continue
-    if (root === null) return [];
-    if (root.left === null) return [];
-
-    // return [root.data].concat(this.leveOrder(root.left), this.leveOrder(root.right));
-    return [root.data, root.left.data, root.right.data].concat(this.leveOrder(root.left), this.leveOrder(root.right))
-  }
-
-
-  test(root){
-    const queue = [];
-    const result = [];
-
-    if (root == null) return;
-
-    queue.push(root);
-
-    while (queue.length > 0) {
-      let current = queue.shift(root);
-      result.push(current.data);
-
-      if (current.left !== null) queue.push(current.left);
-      if (current.right !== null) queue.push(current.right);
-    }
-
-    return result;
-  }
-
-
-}
-
 function removeDuplicates(array) {
   return array.filter((value, index) => array.indexOf(value) === index);
 }
@@ -152,10 +31,132 @@ function prettyPrint(node, prefix = "", isLeft = true) {
   }
 }
 
-let x = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 15];
+
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    this.array = mergeSort(removeDuplicates(array));
+    this.root = this.buildTree(this.array);
+  }
+
+  buildTree(array) {
+    if (array.length < 1) return null;
+
+    const middlePoint = Math.floor((array.length - 1) / 2);
+    const left = array.slice(0, middlePoint);
+    const right = array.slice(middlePoint + 1);
+    const node = new Node(array[middlePoint]);
+
+    node.right = this.buildTree(right);
+    node.left = this.buildTree(left);
+
+    return node;
+  }
+
+  insert(value) {
+    // Check if the value is 0 or undefined and return error
+    if (value === 0 || value === undefined)
+      return console.error("0, minus integers and undefined arent allowed");
+
+    let temp = this.root;
+    let previous = null;
+
+    // Bis Temp nicht null ist
+    while (temp !== null) {
+      // Wenn Temp value ist gleich inserted Value print error
+      if (temp.data === value)
+        return console.error("Value exists already in the Tree");
+      // speichere temp für später
+      previous = temp;
+      // Ändere temp, wenn data > als gesuchte value dann geht man links anonsten recht
+      temp = temp.data > value ? temp.left : temp.right;
+    }
+
+    // Check if value links oder rechts mach eine neue node und mache einen pointer auf diese node
+    if (previous.data > value) {
+      previous.left = new Node(value);
+    } else {
+      previous.right = new Node(value);
+    }
+  }
+
+  delete(value) {
+    // Check if the value is 0 or undefined and return errro
+    if (value === 0 || value === undefined)
+      return console.error("0, minus integers and undefined arent allowed");
+
+    let temp = this.root;
+    let previous = null;
+
+    while (temp !== null) {
+      if (temp.data === value) break;
+      previous = temp;
+      temp = temp.data > value ? temp.left : temp.right;
+    }
+
+    // schauen wo die zahl ist rechts oder links und elete einfach den pointer auf die Node
+    if (previous.data > value) {
+      previous.left = null;
+    } else {
+      previous.right = null;
+    }
+  }
+
+  find(value) {
+    // Check if the value is 0 or undefined and return errro
+    if (value === 0 || value === undefined)
+      return console.error("0, minus integers and undefined arent allowed");
+
+    let temp = this.root;
+    let previous = null;
+
+    while (temp !== null) {
+      if (temp.data === value) break;
+      previous = temp;
+      temp = temp.data > value ? temp.left : temp.right;
+    }
+
+    return temp;
+  }
+
+  height(value){
+    if (value == null) return 0;
+    else
+      {
+      /* compute height of each subtree */
+      let lheight = this.height(value.left);
+      let rheight = this.height(value.right);
+
+
+      console.log(lheight, rheight, value)
+
+      /* use the larger one */
+      if (lheight > rheight)
+          return (lheight + 1);
+      else
+        return (rheight + 1);
+      }
+  }
+
+  printhey(){
+    console.log("hey")
+  }
+
+
+}
+
+
+
+let x = [ 2, 7, 4, 23, 8, 12, 4, 3, 5, 7, 9, 6, 67, 350, 324, 15];
 let test = new Tree(x);
 
 prettyPrint(test.root);
-
-
-console.log(test.test(test.root))
+console.log(test.height(test.root))
